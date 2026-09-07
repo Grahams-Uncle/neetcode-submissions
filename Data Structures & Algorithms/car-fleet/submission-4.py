e198b4b8c4ee638dd1d067288b556eb139fa9dab
Xinchang Xiong <68521327+Grahams-Uncle@users.noreply.github.com>
2026-09-07 14:58:56 -0400

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = [(p,s) for p, s in zip(position, speed)]
+        cars.sort(reverse=True) # Largest first
+        stack = []
+
+        for car in cars:
+            time = abs(target - car[0]) / car[1]
+            if stack and time <= stack[-1]:
+                continue 
+            stack.append(time)
+        return len(stack)
+
+
